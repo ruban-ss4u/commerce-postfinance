41a146e684e032614952ce57fcbb77a89a0ce2b9
ruban-ss4u <103740642+ruban-ss4u@users.noreply.github.com>
2022-05-05 10:00:13 +0530

    Update Gateway.php

--- src/gateways/Gateway.php
+++ src/gateways/Gateway.php
@@ -11,6 +11,7 @@ use craft\commerce\errors\PaymentException;
 use craft\commerce\helpers\Currency;
 use craft\commerce\models\Address;
 use craft\commerce\models\payments\BasePaymentForm;
+use craft\commerce\stripe\models\forms\payment\Charge as PaymentForm;
 use craft\commerce\models\payments\OffsitePaymentForm;
 use craft\commerce\models\PaymentSource;
 use craft\commerce\models\Transaction;
@@ -27,6 +28,17 @@ use craft\web\View;
 
 class Gateway extends BaseGateway
 {
+
+    /**
+     * @var string
+     */
+    public $apiUsername;
+
+    /**
+     * @var string
+     */
+    public $apiPassword;
+
     public static function displayName(): string
     {
         return Craft::t('commerce-postfinance', 'PostFinance Checkout');
@@ -34,6 +46,11 @@ class Gateway extends BaseGateway
 
     public function getPaymentFormHtml(array $params)
     {
+        $params = [
+            'gateway' => $this,
+            'paymentForm' => $this->getPaymentFormModel(),
+        ];
+
         $view = Craft::$app->getView();
 
         $previousMode = $view->getTemplateMode();
@@ -43,5 +60,138 @@ class Gateway extends BaseGateway
         $view->setTemplateMode($previousMode);
 
         return $html;
+    }
+
+    public function getSettingsHtml()
+    {
+        return Craft::$app->getView()->renderTemplate('commerce-postfinance/settings', ['gateway' => $this]);
+    }
+
+    public function completePurchase(Transaction $transaction): RequestResponseInterface
+    {
+        // To do
+    }
+
+    public function capture(Transaction $transaction, string $reference): RequestResponseInterface
+    {
+        // To do
+    }
+
+    public function createPaymentSource(BasePaymentForm $sourceData, int $userId): PaymentSource
+    {
+        // To do
+    }
+
+    public function deletePaymentSource($token): bool
+    {
+        // To do
+    }
+
+    public function getPaymentFormModel(): BasePaymentForm
+    {
+        // To do
+    }
+
+    public function refund(Transaction $transaction): RequestResponseInterface
+    {
+        // To do
+    }
+
+    public function authorize(Transaction $transaction, BasePaymentForm $form): RequestResponseInterface
+    {
+        // To do
+    }
+
+    public function completeAuthorize(Transaction $transaction): RequestResponseInterface
+    {
+        // To do
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function purchase(Transaction $transaction, BasePaymentForm $form): RequestResponseInterface
+    {   
+        // To do
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function processWebHook(): WebResponse
+    {
+        // To do
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function supportsCapture(): bool
+    {
+        // To do
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function supportsAuthorize(): bool
+    {
+        // To do
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function supportsCompleteAuthorize(): bool
+    {
+        // To do
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function supportsCompletePurchase(): bool
+    {
+        // To do
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function supportsPaymentSources(): bool
+    {
+        // To do
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function supportsPurchase(): bool
+    {
+        // To do
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function supportsRefund(): bool
+    {
+        // To do
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function supportsPartialRefund(): bool
+    {
+        // To do
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function supportsWebhooks(): bool
+    {
+        return true;
     }
 }
