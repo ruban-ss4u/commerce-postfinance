b0a62917339552b5dfe991ee7518c50fcd1d1fc5
ruban-ss4u <103740642+ruban-ss4u@users.noreply.github.com>
2022-05-27 19:04:25 +0530

    Add files via upload

---
+++ src/assets/PaymentFormAsset.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace craft\commerce\postfinance\assets;
+
+use craft\web\AssetBundle;
+
+/**
+ * Asset bundle for the Payment Form
+ */
+class PaymentFormAsset extends AssetBundle
+{
+    /**
+     * @inheritdoc
+     */
+    const ASSETPATH = __DIR__;
+    public function init()
+    {
+        $this->sourcePath = __DIR__;
+    
+        $this->css = [
+            'css/paymentForm.css',
+        ];
+
+        parent::init();
+    }
+}
